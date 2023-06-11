df0d0fe0df901082d48bafdcbe0260b9afe05a1c
Yuliia2280 <yulyasosnova@gmail.com>
2023-06-11 22:55:06 +0200

    Added the ability to display the current weather and its forecast for 6 days in two units of measurement: metric and imperial.
     When radio button "℃" is selected, unit="metric" parameter is used (temperature is displayed in Celsius, wind speed is in meter/sec), when radio button "℉" is selected, unit="imperial" parameter is used (temperature is displayed in Fahrenheit, speed is wind-in miles/hour)

--- src/index.js
+++ src/index.js
@@ -47,28 +47,8 @@ function formatDay(timestamp) {
 
 function displayForecast(response) {
   let forecast = response.data.daily;
-  console.log(forecast);
-
+  //console.log(forecast);
   let forecastElement = document.querySelector("#forecast");
-  /*
-  let currentDate = new Date(local);
-  let index = currentDate.getDay();
-  let i = 0;
-  let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-  let daysTransform = [];
-  console.log(daysTransform);
-  do {
-    if (index < 7) {
-      daysTransform[i] = days[index];
-    } else {
-      index = 0;
-      daysTransform[i] = days[index];
-    }
-    i = i + 1;
-
-    index = index + 1;
-  } while (i < 6);
-*/
   let forecastHTML = `<div class="row mt-5">`;
 
   forecast.forEach(function (forecastDay, index) {
@@ -128,16 +108,14 @@ function correctSpellingCityName(city) {
 
 function getForecast(coordinates) {
   let apiKey = "445905dadb3d2b0c6f1b916c9d0e3860";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${unit}`;
   axios.get(`${apiUrl}`).then(displayForecast);
 }
 
 function showTemp() {
   let cityInput = document.querySelector("#city-input").value;
   let apiKey = "445905dadb3d2b0c6f1b916c9d0e3860";
-  //let apiKey = "25fad9f7e87157d33dde0f82ab269ee8";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=${unit}`;
   axios.get(`${apiUrl}`).then(displayWeatherCondition);
 }
 
@@ -147,14 +125,17 @@ function displayWeatherCondition(response) {
   let description = response.data.weather[0].description.toUpperCase();
   let humidity = response.data.main.humidity;
   let wind = Math.round(response.data.wind.speed);
-  let temperature = Math.round(response.data.main.temp);
   local = (response.data.dt + response.data.timezone) * 1000;
   celsiusTemperature = Math.round(response.data.main.temp);
   document.querySelector("#city-input").value = `${city}`;
   document.querySelector(".city").innerHTML = `${city}, ${country}`;
   document.querySelector("#description").innerHTML = `${description}`;
   document.querySelector("#humidity").innerHTML = `${humidity}`;
-  document.querySelector("#wind").innerHTML = `${wind}`;
+  if (unit === "metric") {
+    document.querySelector("#wind").innerHTML = `${wind} meter/sec`;
+  } else {
+    document.querySelector("#wind").innerHTML = `${wind} miles/hour`;
+  }
   document.querySelector("#day").innerHTML = formatDate(local);
   document
     .querySelector("#icon")
@@ -165,7 +146,7 @@ function displayWeatherCondition(response) {
   document
     .querySelector("#icon")
     .setAttribute("alt", `${response.data.weather[0].description}`);
-  document.querySelector("#flexRadioCelsius").checked = true;
+
   if (celsiusTemperature > 0) {
     document.querySelector("#currentTemp").innerHTML = `+${celsiusTemperature}`;
   } else {
@@ -175,33 +156,25 @@ function displayWeatherCondition(response) {
 }
 
 function showTempCelsius() {
-  let current_temp = document.querySelector("#currentTemp");
-  if (celsiusTemperature > 0) {
-    current_temp.innerHTML = `+${celsiusTemperature}`;
-  } else {
-    current_temp.innerHTML = `${celsiusTemperature}`;
-  }
+  unit = "metric";
+  document.querySelector("#flexRadioCelsius").checked = "true";
+  searchCity(cityInput.value);
 }
+
 function showTempFahrenheit() {
-  let current_temp = document.querySelector("#currentTemp");
-  if (celsiusTemperature > 0) {
-    current_temp.innerHTML = `+${Math.round(
-      (celsiusTemperature * 9) / 5 + 32
-    )}`;
-  } else {
-    current_temp.innerHTML = `${Math.round((celsiusTemperature * 9) / 5 + 32)}`;
-  }
+  unit = "imperial";
+  document.querySelector("#flexRadioFahrenheit").checked = "true";
+  searchCity(cityInput.value);
 }
 
 function searchCity(city) {
   let apiKey = "445905dadb3d2b0c6f1b916c9d0e3860";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
   axios.get(`${apiUrl}`).then(displayWeatherCondition);
 }
 
 function searchCityInput(event) {
   event.preventDefault();
-  let cityInput = document.querySelector("#city-input");
   cityInput.value = cityInput.value.toLowerCase();
   cityInput.value = remove_extra_char_spaces_within_a_string(cityInput.value);
   cityInput.value = correctSpellingCityName(cityInput.value);
@@ -211,8 +184,7 @@ function searchCityInput(event) {
 function showWeather_currentLocation(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
-  let unit = "metric";
-  let apiKey = "25fad9f7e87157d33dde0f82ab269ee8";
+  let apiKey = "445905dadb3d2b0c6f1b916c9d0e3860";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unit}`;
   axios.get(`${apiUrl}`).then(displayWeatherCondition);
 }
@@ -224,6 +196,10 @@ function searchLocation(event) {
 
 let celsiusTemperature = null;
 let local = null;
+let unit = "metric";
+let cityInput = document.querySelector("#city-input");
+searchCity("Kyiv");
+
 document
   .querySelector("#id-search-form")
   .addEventListener("submit", searchCityInput);
@@ -233,11 +209,9 @@ document
   .addEventListener("click", searchLocation);
 
 document
-  .querySelector("#flexRadioCelsius")
-  .addEventListener("click", showTempCelsius);
-
-document
   .querySelector("#flexRadioFahrenheit")
   .addEventListener("click", showTempFahrenheit);
 
-searchCity("Kyiv");
+document
+  .querySelector("#flexRadioCelsius")
+  .addEventListener("click", showTempCelsius);
